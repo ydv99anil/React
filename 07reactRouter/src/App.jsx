c211d170d16d87f2e847f086f733f5bdacd5291f
ydv99anil <ydv99anil@gmail.com>
2025-02-22 13:08:07 +0530

    Added README.md file

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-green-700 text-5xl text-center p-3 w-1/2 m-auto mt-3 font-bold'>React Router</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+
+const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        {
+          path: "",
+          element: <Home />
+        },
+        {
+          path: "About",
+          element: <About />
+        }
+      ]
+    }
+  ]
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
